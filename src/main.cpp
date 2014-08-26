bda86f78fff581c945792072df8f67681f8cfa36
Anirudh <m.anirudh94@gmail.com>
2014-08-26 17:55:03 -0400

    keeps threshold after detecting skin

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,22 @@ int dilation_elem = 0;
 int dilation_size =5;
 int const max_elem = 2;
 int const max_kernel_size = 21;
+Scalar avgSkinCol[6];
+int numBox = 6;
 
+//create elements for morphological tranforms
+Mat eroElement = getStructuringElement( MORPH_RECT,
+			   Size( 2*erosion_size + 1, 2*erosion_size+1 ),
+			   Point( erosion_size, erosion_size ) );
 
-Mat detectHand(VideoCapture cap);
+Mat dilElement = getStructuringElement( MORPH_RECT,
+					   Size( 2*dilation_size + 1, 2*dilation_size+1 ),
+					   Point( dilation_size, dilation_size ) );
+
+Mat filterHand(VideoCapture cap);
 Scalar detectSkinColor(Mat frame, Rect scanBox);
-Mat scanBox(Rect box, Mat frame);
+Mat scanBox(Rect box, Mat frame, int boxNum);
+void detectHand(Mat frame);
 
 int main(){
 	Mat frame;
@@ -31,127 +42,71 @@ int main(){
 	if(!cap.isOpened())
 		return -1;
 
-	frame = detectHand(cap);
+	frame = filterHand(cap);
+
+	detectHand(frame);
 	//HSVMethod();
-
-	 Mat canny_output, src_gray;
-	  vector<vector<Point> > contours;
-	  vector<Vec4i> hierarchy;
-
-	  /// Find contours
-	  findContours( frame, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-
-	  /// Draw contours
-	  Mat drawing = Mat::zeros( frame.size(), CV_8UC3 );
-	  for( int i = 0; i< contours.size(); i++ )
-	     {
-	       Scalar color = Scalar(0,0,255);
-	       drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-	     }
-
-	  /// Show in a window
-	  namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-	  imshow( "Contours", drawing );
 
 	waitKey(0);
 
 	return 0;
 }
 
-Mat detectHand(VideoCapture cap){
-	Mat finFrame;
+void detectHand(Mat frame){
 
+}
+
+Mat filterHand(VideoCapture cap){
+	Mat finFrame;
 
     while(1){
     	Mat frame, finImage, showFrame, channel[3], threshImage[6];
-		Scalar avgSkinCol[3], lowerCol, upperCol;
 
+		//get frame from camera
 		cap >> frame;
 		flip(frame, frame, 1);
 
+		//make a copy
 		frame.copyTo(showFrame);
 
 		//soften image
 		GaussianBlur(frame, frame, Size(5,5), 0);
 
-		//remove noise
-		medianBlur(frame, frame, 5);
 		cvtColor(frame, frame, CV_BGR2HSV);
 
-		//determine dimensions
+		//Create ROIs
 		int square = 25;
+		Rect box1(frame.cols/2 - square/2, frame.rows/2 - (5*square/2),square, square); //up
+ 		Rect box2(frame.cols/2 - square/2, frame.rows/2 - square/2, square, square); //mid
+		Rect box3(frame.cols/2 - 2*square, frame.rows/2 - (square/2), square, square); //left mid
+		Rect box4(frame.cols/2 - 2*square, frame.rows/2 + (3*square/2), square, square); //left low
+		Rect box5(frame.cols/2 + square, frame.rows/2 - (square/2), square, square); //right mid
+		Rect box6(frame.cols/2 + square, frame.rows/2 + (3*square/2), square, square); //right low
 
-		//upper
-		Rect box1(frame.cols/2 - square/2,
-					 frame.rows/2 - (square/2),
-					 square,
-					 square);
+		//threshold the images
+		threshImage[0] = scanBox(box1,frame, 0);
+		threshImage[1] = scanBox(box2,frame, 1);
+		threshImage[2] = scanBox(box3,frame, 2);
+		threshImage[3] = scanBox(box4,frame, 3);
+		threshImage[4] = scanBox(box5,frame, 4);
+		threshImage[5] = scanBox(box6,frame, 5);
 
-		//middle
-		Rect box2(frame.cols/2 - square/2,
-					 frame.rows/2 - square/2,
-					 square,
-					 square);
-
-		//left middle
-		Rect box3(frame.cols/2 - 2*square,
-					 frame.rows/2 - (square/2),
-					 square,
-					 square);
-
-		//left lower
-		Rect box4(frame.cols/2 - 2*square,
-				 frame.rows/2 + (3*square/2),
-				 square,
-				 square);
-
-		//right middle
-		Rect box5(frame.cols/2 + square,
-							 frame.rows/2 - (square/2),
-							 square,
-							 square);
-
-		Rect box6(frame.cols/2 + square,
-							 frame.rows/2 + (3*square/2),
-							 square,
-							 square);
-
-		Rect box7(frame.cols/2 - square,
-					 frame.rows/2 + (3*square/2),
-					 square,
-					 square);
-
-		threshImage[0] = scanBox(box1,frame);
-		//threshImage[1] = scanBox(box2,frame);
-		//threshImage[2] = scanBox(box3,frame);
-		//threshImage[3] = scanBox(box4,frame);
-		//threshImage[4] = scanBox(box5,frame);
-		//threshImage[5] = scanBox(box6,frame);
-
+		//combine
 		finImage = threshImage[0] | threshImage[1] | threshImage[2] | threshImage[3] | threshImage[4] | threshImage[5];
-
-
-		Mat element1 = getStructuringElement( MORPH_RECT,
-					   Size( 2*erosion_size + 1, 2*erosion_size+1 ),
-					   Point( erosion_size, erosion_size ) );
-
-		Mat element2 = getStructuringElement( MORPH_RECT,
-							   Size( 2*dilation_size + 1, 2*dilation_size+1 ),
-							   Point( dilation_size, dilation_size ) );
 
 		//get rid of excess noise
 		medianBlur(finImage,finImage,5);
 
 		//opening morphological transform
-		erode( finImage, finImage, element1);
-		dilate( finImage, finImage, element2);
+		erode( finImage, finImage, eroElement);
+		dilate( finImage, finImage, dilElement);
 
 		rectangle(showFrame,box1,Scalar(0,0,255),2,8,0);
-		//rectangle(showFrame,box2,Scalar(0,0,255),2,8,0);
-		//rectangle(showFrame,box3,Scalar(0,0,255),2,8,0);
-		//rectangle(showFrame,box4,Scalar(0,0,255),2,8,0);
-		//rectangle(showFrame,box5,Scalar(0,0,255),2,8,0);
-		//rectangle(showFrame,box6,Scalar(0,0,255),2,8,0);
+		rectangle(showFrame,box2,Scalar(0,0,255),2,8,0);
+		rectangle(showFrame,box3,Scalar(0,0,255),2,8,0);
+		rectangle(showFrame,box4,Scalar(0,0,255),2,8,0);
+		rectangle(showFrame,box5,Scalar(0,0,255),2,8,0);
+		rectangle(showFrame,box6,Scalar(0,0,255),2,8,0);
 
 		//display frame
 		namedWindow("frame", CV_WINDOW_AUTOSIZE);
@@ -163,25 +118,70 @@ Mat detectHand(VideoCapture cap){
 
         if(waitKey(30) >= 0){
         	finFrame = finImage;
+
         	break;
         }
 
     }
 
+    while(1){
+        	Mat frame, finImage, showFrame, threshImage[6];
+    		Scalar lowerCol, upperCol;
+
+    		//get frame from camera
+    		cap >> frame;
+    		flip(frame, frame, 1);
+
+    		//soften image
+    		GaussianBlur(frame, frame, Size(5,5), 0);
+
+    		cvtColor(frame, frame, CV_BGR2HSV);
+
+    		//threshold the images
+    		for(int i = 0; i<numBox; i++){
+
+        		inRange(frame,
+        				Scalar(avgSkinCol[i][0]-20,avgSkinCol[i][1]-30,avgSkinCol[i][2]-20), //0,55,90
+        				Scalar(avgSkinCol[i][0]+20,avgSkinCol[i][1]+30,avgSkinCol[i][2]+20), //28,175,230
+        				threshImage[i]
+        		);
+
+        		medianBlur(threshImage[i], threshImage[i], 5);
+    		}
+
+    		//combine
+    		finImage = threshImage[0] | threshImage[1] | threshImage[2] | threshImage[3] | threshImage[4] | threshImage[5];
+
+    		//get rid of excess noise
+    		medianBlur(finImage,finImage,11);
+
+    		//opening morphological transform
+    		erode( finImage, finImage, eroElement);
+    		dilate( finImage, finImage, dilElement);
+
+    		//show results
+    		namedWindow("finImage", CV_WINDOW_AUTOSIZE);
+    		imshow("finImage", finImage);
+
+            if(waitKey(30) >= 0){
+            	finFrame = finImage;
+            	break;
+            }
+
+        }
     return finFrame;
 }
 
-Mat scanBox(Rect box, Mat frame){
+Mat scanBox(Rect box, Mat frame, int boxNum){
 	Mat threshImage;
-	Scalar avgSkinCol;
 
-	avgSkinCol = detectSkinColor(frame, box);
+	avgSkinCol[boxNum] = detectSkinColor(frame, box);
 
 	//GaussianBlur(frame, frame, Size(5,5), 0);
 	//threshold skin
 	inRange(frame,
-			Scalar(avgSkinCol[0]-20,avgSkinCol[1]-30,140),
-			Scalar(avgSkinCol[0]+20,avgSkinCol[1]+30,220),
+			Scalar(avgSkinCol[boxNum][0]-20,avgSkinCol[boxNum][1]-30,avgSkinCol[boxNum][2]-20),
+			Scalar(avgSkinCol[boxNum][0]+20,avgSkinCol[boxNum][1]+30,avgSkinCol[boxNum][2]+20),
 			threshImage);
 
 	medianBlur(threshImage, threshImage, 5);
